fe1dbeee1db3ac0a7bf91d3dceb1b8d45d0ce216
Erick <erickson.998070@gmail.com>
2020-06-24 23:11:11 -0500

    se genera ya contraseña alfanumerica

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,52 @@
-var btnGenerar = document.getElementById('generar');
-var mensaje = document.querySelector('.alerta');
-
-
+var btnGenerar = document.getElementById("generar");
+var mensaje = document.querySelector(".alerta");
 
 btnGenerar.onclick = function generarPassword() {
-    mensaje.removeAttribute('hidden');
-    console.log('click');
+    mensaje.removeAttribute("hidden");
+    generar();
+    console.log("click");
+};
+
+function generar() {
+    let password = [],
+        letras = [
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n",
+            "o",
+            "p",
+            "q",
+            "r",
+            "s",
+            "t",
+            "u",
+            "v",
+            "w",
+            "z",
+            "y",
+            "z",
+        ];
+
+    for (let i = 0; i < 15; i++) {
+        let con = Math.random();
+        let numL = Math.round(Math.random() * (25 - 0) + 0);
+        let num = Math.round(Math.random() * (9 - 0) + 0);
+        if (con < 0.8) {
+            password.push(letras[numL]);
+        } else {
+            password.push(num);
+        }
+    }
+    console.log(password);
 }
