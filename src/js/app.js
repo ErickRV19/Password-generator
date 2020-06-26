3918129ea494e57a71390c1319fb600462f6a255
Erick <erickson.998070@gmail.com>
2020-06-26 13:17:07 -0500

    ya se genera contraseñas

--- src/js/app.js
+++ src/js/app.js
@@ -3,13 +3,14 @@ var btncaracter8 = document.getElementById("ochoCarac");
 var btncaracter15 = document.getElementById("carac");
 var mensaje = document.querySelector(".alerta");
 var spacePassword = document.querySelector(".password");
+var checkMayuscula = document.getElementById("checkMayuscula");
+var checkSimbolos = document.getElementById("checkSimbolos");
 let carac = 0,
     aux = 0;
 
 btnGenerar.onclick = function generarPassword() {
     mensaje.removeAttribute("hidden");
     generar();
-    console.log("click");
 };
 
 btncaracter8.onclick = function caracter8() {
@@ -50,21 +51,61 @@ function generar() {
             "z",
             "y",
             "z",
+        ],
+        simbolo = [
+            "!",
+            "#",
+            "$",
+            "%",
+            "&",
+            "(",
+            ")",
+            "*",
+            "+",
+            "-",
+            "/",
+            "<",
+            "=",
+            ">",
+            "?",
+            "@",
+            "[",
+            "]",
+            "{",
+            "|",
+            "}",
         ];
     if (aux == 0) {
         carac = 15;
     }
-
     for (let i = 0; i < carac; i++) {
         let con = Math.random();
         let numL = Math.round(Math.random() * (25 - 0) + 0);
         let num = Math.round(Math.random() * (9 - 0) + 0);
-        if (con < 0.8) {
-            password.push(letras[numL]);
-        } else {
-            password.push(num);
+        if (num < 7) {
+            if (checkMayuscula.checked) {
+                if (num < 4) {
+                    password.push(letras[numL].toUpperCase());
+                } else {
+                    password.push(letras[numL]);
+                }
+            } else {
+                password.push(letras[numL]);
+            }
+        }
+        if (num >= 7) {
+            if (checkSimbolos.checked) {
+                if (con == 0) {
+                    password.push(num);
+                } else {
+                    password.push(simbolo[numL]);
+                }
+            } else {
+                password.push(num);
+            }
         }
     }
+
     console.log(password);
     spacePassword.innerHTML = password.join("");
 }
